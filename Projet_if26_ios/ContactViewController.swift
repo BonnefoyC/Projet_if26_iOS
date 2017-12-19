18551f2359bb6ed2a212bb497cc33ecfe8ff028f
BonnefoC <clementbonnefoy1@gmail.com>
2017-12-19 15:59:30 +0100

    Vérification envoi de mail.

--- Projet_if26_ios/ContactViewController.swift
+++ Projet_if26_ios/ContactViewController.swift
@@ -12,6 +12,7 @@ class ContactViewController: UIViewController {
 
     @IBOutlet weak var labelContact: UILabel!
     
+    @IBOutlet weak var bt_message: UIButton!
     @IBOutlet weak var et_nom: UITextField!
     
     @IBOutlet weak var et_message: UITextView!
@@ -39,6 +40,60 @@ class ContactViewController: UIViewController {
     }
     
     @IBAction func buttonClick(_ sender: UIButton) {
+        
+        
+        if(verification()) {
+            
+            envoyerMail()
+            
+            let message = "Votre message a bien été envoyé."
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            self.present(alert, animated: true)
+            
+            // duration in seconds
+            let duration: Double = 5
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + duration) {
+                alert.dismiss(animated: true)
+            }
+            
+            et_nom.isEnabled = false
+            et_mail.isEnabled = false
+            et_message.isEditable = false
+            bt_message.isEnabled = false
+        }
+        
+    }
+    
+    func verification() -> Bool {
+        
+        var check : Bool = true
+        
+        if(et_message.text == "") {
+            check = false
+            et_message.backgroundColor = UIColor.red
+        } else {
+            et_message.backgroundColor = UIColor.white
+        }
+        if(et_nom.text == "") {
+            check = false
+            et_nom.backgroundColor = UIColor.red
+        }else {
+            et_nom.backgroundColor = UIColor.white
+        }
+        if(et_mail.text == "") {
+            check = false
+            et_mail.backgroundColor = UIColor.red
+        }else {
+            et_mail.backgroundColor = UIColor.white
+        }
+        
+        return check
+    }
+    
+    func envoyerMail() {
+        
+        //TO DO
         
     }
     
